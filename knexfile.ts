aea844cb6c5ac834ea1f847d272b6d3132285404
Iago Carvalho <iagocarvalhoweb@gmail.com>
2020-08-29 18:43:28 -0300

    terminando criação de tabelas no banco de dados - mudança de sqlite para postgres

--- knexfile.ts
+++ knexfile.ts
@@ -1,13 +1,37 @@
+// Update with your config settings.
 import path from "path";
-
 module.exports = {
-  client: "sqlite3",
-  connection: {
-    filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "server_week",
+      user: "postgres",
+      password: "536261",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
+    useNullAsDefault: true,
   },
-  migrations: {
-    directory: path.resolve(__dirname, "src", "database", "migrations"),
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
   },
 };
-
-// npx knex migrate:lastest --knexfile knexfile.ts migration:latest
